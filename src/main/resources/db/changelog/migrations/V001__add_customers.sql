b2691a4979873d929b58da25ddfca1e1c728a877
Александр Горшков <stollprog@mail.ru>
2022-11-19 13:59:01 +0300

    add feature migrations

---
+++ src/main/resources/db/changelog/migrations/V001__add_customers.sql
@@ -0,0 +1,5 @@
+INSERT INTO  customers ( name, surname, age, phone_number) VALUES
+( 'Ivan', 'Sidorov', 29, '1321354654'),
+('Alex', 'Gorshkov', 43, '6546546542'),
+('Roman', 'Frolov', 26, '1325489654'),
+('Elena', 'Blinova', 33, '879653132');
---
+++ src/main/resources/db/changelog/migrations/V001__add_orders.sql
@@ -0,0 +1,7 @@
+INSERT INTO orders (date, customer_id, product_name, amount) VALUES
+    ('2022-11-16', 1, 'tv', 1),
+    ('2022-11-13', 2, 'notebook', 1),
+    ('2022-10-12', 3, 'smartphone', 1),
+    ('2022-11-03', 1, 'cpu', 1),
+    ('2022-09-15', 2, 'graphics card', 1),
+    ('2022-09-11', 3, 'ram', 2)
